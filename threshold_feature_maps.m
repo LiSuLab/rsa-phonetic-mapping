a152c2261a6c359aa290f975923348d943f035da
Cai Wingfield <cai@caiwingfield.com>
2015-07-14 14:37:50 +0100

    Get indices of the super-threshold vertices.

--- threshold_feature_maps.m
+++ threshold_feature_maps.m
@@ -1,13 +1,19 @@
-function thresholded_paths = threshold_feature_maps(feature_paths_ea, feature_thresholds, userOptions)
+function [thresholded_paths, threshold_vis] = threshold_feature_maps(feature_paths_ea, feature_thresholds, userOptions)
 
     meshes_dir = fullfile(userOptions.rootPath, 'Meshes');
 
+    % Initialise.
     thresholded_paths = struct();
+    threshold_vis = struct();
 
     features = fieldnames(feature_thresholds)';
     
     for feature = features
        feature = feature{1}; %#ok<FXSET> % unwrap
+       
+       % Initialise inside fields.
+       thresholded_paths.(feature) = struct();
+       threshold_vis.(feature) = struct();
        
        for chi = fieldnames(feature_thresholds.(feature))'
            chi = chi{1}; %#ok<FXSET> % unwrap
@@ -22,12 +28,14 @@ function thresholded_paths = threshold_feature_maps(feature_paths_ea, feature_th
                ...% fixing at some point.
                feature_paths_ea.(feature).([chi 'ea']));
            
-           data = unthresholded_snapshot.data;
+           % We read in a snapshot, so we can just take out one copy of it.
+           data = squeeze(unthresholded_snapshot.data(1,:));
            
-           % Apply the threshold
+           % Get out the vertex indices of the super-threshold vertices
+           threshold_vis.(feature).(chi) = find(data > threshold);
+           
+           % Apply the threshold and write it out again.
            data(data < threshold) = 0;
-           
-           % Write it out again
            rsa.meg.write_stc_snapshot(unthresholded_snapshot, data, thresholded_paths.(feature).(chi));
            
        end
