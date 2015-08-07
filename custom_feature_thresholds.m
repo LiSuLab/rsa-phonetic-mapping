f7fc65da99a9af129e1cfafbc2039c39039b8eca
Cai Wingfield <cai@caiwingfield.com>
2015-08-07 13:18:57 +0100

    Fix custom thresholds for hmm version.

--- custom_feature_thresholds.m
+++ custom_feature_thresholds.m
@@ -5,40 +5,43 @@ function thresholds = custom_feature_thresholds()
 
     thresholds = struct();
     
-    thresholds.DORSAL.L = 1;
-    thresholds.DORSAL.R = 2;
+    thresholds.DORSAL.L = 0;
+    thresholds.DORSAL.R = 1;
 
-    thresholds.CORONAL.L = 1;
+    thresholds.CORONAL.L = 0;
     thresholds.CORONAL.R = 0;
 
-    thresholds.LABIAL.L = 1;
-    thresholds.LABIAL.R = 1;
+    thresholds.LABIAL.L = 2;
+    thresholds.LABIAL.R = 0;
 
-    thresholds.HIGH.L = 3;
+    thresholds.HIGH.L = 1;
     thresholds.HIGH.R = 1;
 
-    thresholds.FRONT.L = 1;
+    thresholds.FRONT.L = 2;
     thresholds.FRONT.R = 1;
 
     thresholds.LOW.L = 1;
-    thresholds.LOW.R = 0;
+    thresholds.LOW.R = 1;
 
     thresholds.BACK.L = 1;
-    thresholds.BACK.R = 0;
+    thresholds.BACK.R = 1;
 
     thresholds.PLOSIVE.L = 0;
-    thresholds.PLOSIVE.R = 2;
+    thresholds.PLOSIVE.R = 1;
 
     thresholds.FRICATIVE.L = 0;
     thresholds.FRICATIVE.R = 0;
+    
+    thresholds.SYLLABIC.L = 1;
+    thresholds.SYLLABIC.R = 1;
 
-    thresholds.NASAL.L = 3;
+    thresholds.NASAL.L = 1;
     thresholds.NASAL.R = 1;
 
     thresholds.VOICED.L = 0;
     thresholds.VOICED.R = 1;
 
-    thresholds.OBSTRUENT.L = 1;
+    thresholds.OBSTRUENT.L = 0;
     thresholds.OBSTRUENT.R = 1;
 
     thresholds.SONORANT.L = 0;
